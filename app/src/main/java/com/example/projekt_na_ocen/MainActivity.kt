7bcc67e62555ba4fdb81c363d6be38fc28d71611
PatrykD2pr <patrykdomaszek@gazeta.pl>
2021-10-28 13:29:04 +0200

    Trzeba tylko zamienić zmienne liczba1 i liczba2 na int

--- app/src/main/java/com/example/projekt_na_ocen/MainActivity.kt
+++ app/src/main/java/com/example/projekt_na_ocen/MainActivity.kt
@@ -4,21 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.Napis1).text = "";
+        findViewById<TextView>(R.id.Napis1).text = ""
+        findViewById<TextView>(R.id.Liczba1).text = ""
+        findViewById<TextView>(R.id.Liczba2).text = ""
 
         findViewById<Button>(R.id.Zastapienie).setOnClickListener{
-            findViewById<TextView>(R.id.Napis2).text = findViewById<TextView>(R.id.Napis1).text.toString();
+            findViewById<TextView>(R.id.Napis2).text = findViewById<TextView>(R.id.Napis1).text.toString()
         }
         findViewById<Button>(R.id.polaczanie).setOnClickListener{
-            var text = findViewById<TextView>(R.id.Napis2).text.toString();
-            findViewById<TextView>(R.id.Napis2).text = text.toString() + findViewById<TextView>(R.id.Napis1).text.toString();
+            var text = findViewById<TextView>(R.id.Napis2).text.toString()
+            findViewById<TextView>(R.id.Napis2).text = text.toString() + findViewById<TextView>(R.id.Napis1).text.toString()
+        }
+        findViewById<Button>(R.id.Suma).setOnClickListener{
+            var liczba1 = findViewById<TextView>(R.id.Liczba1).text
+            var liczba2 = findViewById<TextView>(R.id.Liczba2).text
+            var wynik = liczba1.toInt() + liczba2.toInt();
+            findViewById<TextView>(R.id.Wynik_sumy).text = wynik.toInt()
         }
     }
 }
